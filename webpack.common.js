b159c397232e825865c071656fc4cac4d9c231d1
Ali3990 <alan.li.3990@gmail.com>
2026-08-24 18:15:47 -1000

    adjusted webpacks

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    entry: "./src/index.js",
+    entry: "./src/app.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
