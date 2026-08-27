88b0fd3f4f732f6c60b050f29b19cb62eb87035f
Ali3990 <alan.li.3990@gmail.com>
2026-08-26 19:14:30 -1000

    initiate the form generation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log
+
+
+const toDoList = [];
+
+// construction function.
+function task(title, description, dueDate, priority, notes) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+};
+
+function addTaskToList(title, description, dueDate, priority, notes) {
+    const newTask = new task(title, description, dueDate, priority, notes);
+    toDoList.push(newTask)
+};
+
+// handle event when wanting to add task
+
+function createForm() {
+    const form = document.createElement("form");
+    form.id = "task-form";
+    document.querySelector("#task-dialogue").append(form);
+    
+    const titleLabel = document.createElement("label");
+    titleLabel.textContent = "Title: ";
+    titleLabel.htmlFor = ""
+
+};
